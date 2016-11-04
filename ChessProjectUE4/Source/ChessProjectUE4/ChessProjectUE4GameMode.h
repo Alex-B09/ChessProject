d863b61b96795993b51e8348911168317667c53c
Alex-B09 <alex.boulanger@usherbrooke.ca>
2016-11-03 23:07:20 -0400

    Initial UE4 Project commit

---
+++ ChessProjectUE4/Source/ChessProjectUE4/ChessProjectUE4GameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "ChessProjectUE4GameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CHESSPROJECTUE4_API AChessProjectUE4GameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
