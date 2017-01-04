5552313f67db1e8dc8aafccb385d08b7adfe4ff2
Alex-B09 <alex.boulanger@usherbrooke.ca>
2017-01-03 22:57:42 -0500

    Deletion of old actor format. Now all pieces will be directly created in the Editor. The logic will move to the C++ side

--- ChessProjectUE4/Source/ChessProjectUE4/ChessPiecePawn.cpp
+++
@@ -1,9 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "ChessProjectUE4.h"
-#include "ChessPiecePawn.h"
-
-AChessPiecePawn::AChessPiecePawn()
-    :AChessPiece(TEXT("StaticMesh'/Game/Art/Pieces/pawn.pawn'"))
-{
-}
